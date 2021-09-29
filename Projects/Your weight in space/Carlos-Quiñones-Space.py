f3f297fa07adc75768d25a03e3bd2bc2250327c0
CSI-Carlos-Quinones <89105127+CSI-Carlos-Quinones@users.noreply.github.com>
2021-09-29 11:08:01 -0400

    Weight in space Project

---
+++ Projects/Your weight in space/Carlos-Quiñones-Space.py
@@ -0,0 +1,17 @@
+planets = [["Mercury", 3.7],["Venus", 8.87],[ "Earth", 9.81],["Mars", 3.711],["Jupiter", 24.79],[ "Saturn", 10.44], ["Uranus", 8.69], ["Neptune",11.15]]
+
+print(f"{planets[0][0]},{planets[1][0]},{planets[2][0]},{planets[3][0]},{planets[4][0]},{planets[5][0]},{planets[6][0]},{planets[7][0]}")
+
+userLocation = input("So, in which of these planet you at?")
+
+userMass = float(input("What's your mass in kilograms?"))
+
+for planet in planets:
+    if(planet[0] == userLocation):
+      userGravity = planet[1]  
+      
+
+userWeigth  = userGravity * userMass
+
+print(f"Your weigth in {userLocation} is {userWeigth}")
+
