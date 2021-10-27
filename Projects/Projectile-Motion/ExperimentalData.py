567360d7fd43f4000da9520fd7202afd46531548
CSI-Carlos-Quinones <89105127+CSI-Carlos-Quinones@users.noreply.github.com>
2021-10-27 10:27:13 -0400

    no message

--- Projects/Projectile-Motion/ExperimentalData.py
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -1,4 +1,6 @@
+import math
 class ExperimentalData:
+
       def __init__(self, gun, cartridgeCalibre,bulletRound,projectileVelocity,building,buildingHeight,gravity):
           self.gun = gun
           self.cartridgeCalibre = cartridgeCalibre
@@ -7,5 +9,13 @@ class ExperimentalData:
           self.building = building
           self.buildingHeight = buildingHeight
           self.gravity = gravity
+      def getTime(self) :
+            return (round(math.sqrt((2* self.buildingHeight)/self.gravity)))
+      def getDistance(self):
+          return(self.projectileVelocity * self.getTime())
+      def run(self):
+           print(f"If you shoot a {self.bulletRound} with a {self.gun} using a {self.cartridgeCalibre} cartridge from the {self.building} building , that mesures {self.buildingHeight} meters, it will take {self.getTime()} seconds till the bullet traveling at {self.projectileVelocity } m/s lands at {self.getDistance()} meters.")
+
+
           
 #myData = ExperimentalData( "SV-98 bolt-action sniper rifle","7.62x54mmR",865,"7.62x54R LPS gzh,","30 St. Mary Axe", 179.8015,9.8 )
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -25,24 +25,33 @@ import math
 #                         "gravity" : 9.8
 # }
 
-def shoot(experimentalData:ExperimentalData):
+
 
   
-    time = round(math.sqrt((2* 179.8015)/9.8))
-    distanceTraveled = experimentalData.projectileVelocity * time
-    print(f"If you shoot a {experimentalData.bulletRound} with a {experimentalData.gun} using a {experimentalData.cartridgeCalibre} cartridge from the {experimentalData.building} building , that mesures {experimentalData.buildingHeight} meters, it will take {time} seconds till the bullet traveling at {experimentalData.projectileVelocity } m/s lands at {distanceTraveled} meters.")
-{
+    #time = round(math.sqrt((2* experimentalData.buildingHeight)/experimentalData.gravity))
+    #distanceTraveled = experimentalData.projectileVelocity * time
+    
+myDataSet = {
+
  ExperimentalData("SV-98 bolt-action sniper rifle", "7.62x54mmR" ,"7.62x54R LPS gzh",865,"30 St. Mary Axe",179.8015 ,9.8)
-  ,ExperimentalData("Kedr-B 9x18PM submachine gun", "9x18mm Makarov" ,"9x18mm PM P gzh",289,"30 St. Mary Axe",179.8015 ,9.8)
-  ,ExperimentalData("Mk-18 .338 LM marksman rifle", "	.338 Lapua Magnum" ,".338 Lapua Magnum FMJ",890,"30 St. Mary Axe",179.8015 ,9.8)
+  ,ExperimentalData("Kedr-B 9x18PM submachine gun", "9x18mm Makarov" ,"9x18mm PM P gzh",289,"30 St. Mary Axe",179.8015 ,0.38)
+  ,ExperimentalData("Mk-18 .338 LM marksman rifle", "	.338 Lapua Magnum" ,".338 Lapua Magnum FMJ",890,"30 St. Mary Axe",179.8015 ,2.53)
   ,ExperimentalData("Knight's Armament Company SR-25 7.62x51 marksman rifle", "7.62x51mm NATO" ,"7.62x51mm M80",787,"30 St. Mary Axe",179.8015 ,9.8)
-,ExperimentalData("AKS-74UB 5.45x39 assault rifle", "5.45x39mm" ,"5.45x39mm PS gs",730,"30 St. Mary Axe",179.8015 ,9.8)
+,ExperimentalData("AKS-74UB 5.45x39 assault rifle", "5.45x39mm" ,"5.45x39mm PS gs",730,"30 St. Mary Axe",179.8015 ,1.07)
 }
 
-shoot(myData)
+#shoot(myData)
 
 myOutputPath = Path(__file__).parents[0]
 myOutputFilePath = os.path.join(myOutputPath, "ExperimentalData.json")
 
 with open(myOutputFilePath,'w') as outfile:
-   json.dump(myData.__dict__,outfile)
+  json.dump([data.__dict__ for data in myDataSet],outfile)
+
+deserlaize = open(myOutputFilePath,) 
+experimentJson = json.load(deserlaize) 
+
+for e in experimentJson:
+  print("---------------------------------------------")
+  ExperimentalData(**e).run()
+  
