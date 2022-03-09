38e86d5ff6276949892b67e946d074ed94f995f6
CSI-Carlos-Quinones <65362893+CarlosQR@users.noreply.github.com>
2022-03-09 10:08:03 -0400

    1

---
+++ Projects/SnakeGame/Snake.py
@@ -0,0 +1,6 @@
+import pygame# imports the Library used for the display
+pygame.init() #Starts all the modules used for the game
+dis=pygame.display.set_mode((400,300))#Opens the display and sets the bounderies for it
+pygame.display.update()# updates the display (usless for now)
+pygame.quit()#quits the display
+quit()#quit the code as a whole
